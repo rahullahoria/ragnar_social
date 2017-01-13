7b03f0d1a9e7d11d3e2b2ff7ab2ee430ff07decf
rahullahoria <rahul_lahoria@yahoo.com>
2017-01-13 15:16:19 +0530

    added resouces

--- scripts/alexaRankingUpdate.php
+++ scripts/alexaRankingUpdate.php
@@ -10,14 +10,15 @@
 
 $dbHandle = mysqli_connect("localhost","root","redhat@11111p","ragnar_social");
 
-$domains = mysqli_query($dbHandle, "SELECT * FROM `domains` WHERE `status` = 'white'");
+$domains = mysqli_query($dbHandle, "SELECT * FROM `domains` as a INNER JOIN domain_dynamic_details as b
+                          WHERE a.`status` = 'white' and a.id = b.domain_id and DATA(b.creation) != CURDATE() ");
 
 while ( $domain = mysqli_fetch_array($domains)) {
     $url="http://".$domain['name']."/";
     $xml = simplexml_load_file('http://data.alexa.com/data?cli=10&dat=snbamz&url='.$url);
     $rank=isset($xml->SD[1]->POPULARITY)?$xml->SD[1]->POPULARITY->attributes()->TEXT:0;
     $web=(string)$xml->SD[0]->attributes()->HOST;
-    echo $web." has Alexa Rank ".$rank;
+    echo $web." has Alexa Rank ".$rank."\n";
 
     $insertSql = "INSERT INTO `domain_dynamic_details`( `domain_id`, `alexa_ranking`, `creation`)
               VALUES ('".$domain['id']."','".$rank."','".date("Y-m-d H:i:s")."')";
