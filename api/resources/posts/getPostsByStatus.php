d3ea055ec508bf61f2fc1a8841c4e4e0eeb7d6c1
rahullahoria <rahul_lahoria@yahoo.com>
2017-01-24 11:42:01 +0530

    added resouces

--- api/resources/posts/getPostsByStatus.php
+++ api/resources/posts/getPostsByStatus.php
@@ -8,7 +8,7 @@
 
 function getPostsByStatus($orgId, $userId, $type){
 
-    $sql = "SELECT a.id, a.`title`, a.`description`, a.`link`, a.`raw_img_id`, a.`gen_img_id, a.creation, `b.website
+    $sql = "SELECT a.id, a.`title`, a.`description`, a.`link`, a.`raw_img_id`, a.gen_img_id, a.creation, b.website
               FROM posts as a INNER JOIN  companies as b WHERE a.company_id = b.id
               AND  a.company_id=:c_id and a.gen_img_id != 0 and  a.user_id =:user_id and a.status = :status
                   and a.id NOT IN (select post_id from post_tracks where 1)";
