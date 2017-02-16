37aa35ddab019529befe07b6735d6b530a09d73a
rahullahoria <rahul_lahoria@yahoo.com>
2017-02-16 21:23:22 +0530

    added resouces

--- web-site/index.php
+++ web-site/index.php
@@ -1330,6 +1330,16 @@ Campaigning engine for our viewers who would like to get our latest content upda
 <!-- Custom js-->
 <script src="library/js/custom.js"></script>
 <script src="library/js/validation.js"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-92064147-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
 
