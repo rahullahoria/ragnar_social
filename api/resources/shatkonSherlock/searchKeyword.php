91da52c5c3a64b401b036a195b3713304ed0a5af
rahullahoria <rahul_lahoria@yahoo.com>
2017-03-10 19:24:48 +0530

    added resouces

--- api/resources/shatkonSherlock/searchKeyword.php
+++ api/resources/shatkonSherlock/searchKeyword.php
@@ -139,11 +139,11 @@ function getKeywordUrls($keyword,$type,$site){
         try {
 
             $keywordSr = $keyword .($site?'+'.$site:'');
-            str_replace(' ','+',$keywordSr);
+            $keywordSr = str_replace(' ','+',$keywordSr);
             $URL = "https://www.google.co.in/search?q=".$keywordSr."&ie=utf-8&oe=utf-8&gws_rd=cr".$typeMap[$type];
 
             $homepage = file_get_contents($URL);
-            var_dump($homepage);die('page');
+            //var_dump($homepage);die('page');
             $urls = explode('/url?q=',$homepage);
             for($i = 1; $i<count($urls);$i++) {
                 $urls[] = explode('&amp;', $urls[$i])[0];
