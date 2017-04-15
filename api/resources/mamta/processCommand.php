a2c1455e9660ac6a52caedb6d9216622a4ace496
rahullahoria <rahul_lahoria@yahoo.com>
2017-04-16 00:19:10 +0530

    added resouces

--- api/resources/mamta/processCommand.php
+++ api/resources/mamta/processCommand.php
@@ -11,6 +11,6 @@ function processCommand($command){
 
 $url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=".$command;
 
-echo    httpGet($url);
+var_dump(httpGet($url));
 
 }
