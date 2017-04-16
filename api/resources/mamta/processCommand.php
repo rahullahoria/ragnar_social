9572758a9a18ec36e4d486bf2b0b2b0653c2d125
rahullahoria <rahul_lahoria@yahoo.com>
2017-04-16 12:09:06 +0530

    added resouces

--- api/resources/mamta/processCommand.php
+++ api/resources/mamta/processCommand.php
@@ -15,7 +15,7 @@ $url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extract
 $response = json_decode(httpGet($url));
 $pages = $response->query->pages;
     foreach($pages as $value){
-        $reply  = $value->extract;
+        $reply  = htmlspecialchars($value->extract);
         break;
     }
     echo '{"reply": "'.$reply.'"}';
