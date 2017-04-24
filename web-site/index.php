ed81d8a26fbbf0680543a4cb54c5dbdf0674879c
rahullahoria <rahul_lahoria@yahoo.com>
2017-04-24 19:21:26 +0530

    added resouces

--- web-site/index.php
+++ web-site/index.php
@@ -87,6 +87,7 @@ if(isset($_POST['name'])){
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -94,6 +95,7 @@ if(isset($_POST['name'])){
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
 </head>
 <body>
 <!-- BEGAIN THEME STYLE SWITCHER -->
@@ -292,7 +294,9 @@ if(isset($_POST['name'])){
                                 <input type="text" name="name" class="form-control" id="header_form_name" placeholder="Full Name">
                                 <input type="text" name="email" class="form-control" id="header_form_email" onkeyup="nospaces(this)" placeholder="Email Address *">
                                 <input type="text" name="mobile" class="form-control" id="header_form_mobile" onkeyup="nospaces(this)"placeholder="Mobile No. *">
+                                <div class="g-recaptcha" data-sitekey="6LeycR4UAAAAAHh0XsNW8VLpgw3UrrqipVc6Ndqa"></div>
                                 <input type="submit" id="signup_form_submit" class="submit_btn" onclick="signup()" name="submit" value="Get in Touch!" class="btn-medium">
+
                             </form>
 
                         </div>
